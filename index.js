6567d8d81d7a2304e5b6b8b73dd81be2d6d94a41
Aaron Lutas <alutas95@gmail.com>
2021-01-10 21:35:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays() {
+  return"Happy holidays!"
+}
+
+function happyHolidaysTo(name){
+  return`Happy holidays, ${name}!`
+}
+
+function happyCustomHolidayTo(holiday, person) {
+    return`Happy ${holiday}, ${name}`
+}
+
+function holidayCoutdown(holiday, number) {
+  return`It's ${days} days until ${holiday}!`
+}
