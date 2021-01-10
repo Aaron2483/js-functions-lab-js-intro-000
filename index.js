22c4f74bf2d572f3cad9105fe3b9e3a9a54278cb
Aaron Lutas <alutas95@gmail.com>
2021-01-10 21:49:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(name){
 }
 
 function happyCustomHolidayTo(holiday, person) {
-    return`Happy ${holiday}, ${name}`;
+    return `Happy ${holiday}, ${name}`;
 }
 
 function holidayCoutdown(holiday, days) {
